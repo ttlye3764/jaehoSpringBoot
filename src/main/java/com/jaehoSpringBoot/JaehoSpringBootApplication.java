5377e520dcbbbde433adc52a66e382f81d31a754
jaeho <ttlye9967@gmail.com>
2023-02-25 20:26:21 +0900

    springContainer Bean 등록 후 사용

--- src/main/java/com/jaehoSpringBoot/JaehoSpringBootApplication.java
+++ src/main/java/com/jaehoSpringBoot/JaehoSpringBootApplication.java
@@ -15,6 +15,7 @@ import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -39,43 +40,40 @@ public class JaehoSpringBootApplication {
 
 //        SpringApplication.run(JaehoSpringBootApplication.class, args);
 
+        // SpringContainer 를 대표하는 인터페이스: applicationContext
+        // 1. Bean이 뭐가 들어갈지
+        // 2. resource에 접근하는 방법
+        // 3. 이벤트에 뭐 어쩌고 하는 방법 등등.
+
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.refresh();
+
         // tomcat 시작.
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-
-        HelloController helloController = new HelloController();
-
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
              servletContext.addServlet("frontController", new HttpServlet() {
-
-
                  @Override
                  protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-
-
                      // 각종 공통 기능 처리
                      //  - 인증, 보안, 다국어 등.
+
                      if ("/hello".equals(req.getRequestURI()) // 매핑
                              && req.getMethod().equals(HttpMethod.GET.name())) {
 
                          // 바인딩
                          String name = req.getParameter("name");
-
-
+                         HelloController helloController = applicationContext.getBean(HelloController.class);
                          String ret = helloController.hello(name);
 
-                         resp.setStatus(HttpStatus.OK.value());
-                         resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                         resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
                          resp.getWriter().println(ret);
 
                      } else if ("/file".equals(req.getRequestURI())) {
-
                          // 파일 가져올때.
                          MultipartResolver resolver = new CommonsMultipartResolver(req.getServletContext());
                          MultipartHttpServletRequest multipartHttpServletRequest = resolver.resolveMultipart(req);
                          MultipartFile file = multipartHttpServletRequest.getFile("file");
-
-
                      } else {
                          resp.setStatus(HttpStatus.NOT_FOUND.value());
                      }
