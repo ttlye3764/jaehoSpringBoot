bac9e2fcce52936881650e55483e973b2b798b34
jaeho <ttlye9967@gmail.com>
2023-02-26 21:00:24 +0900

    MySpringBootAnnotation anootation 생성 및 config 추가

--- src/main/java/com/jaehoSpringBoot/JaehoSpringBootApplication.java
+++ src/main/java/com/jaehoSpringBoot/JaehoSpringBootApplication.java
@@ -11,19 +11,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
 //@SpringBootApplication
-@Configuration
-@ComponentScan
+@MySpringBootAnnotation
 public class JaehoSpringBootApplication {
-
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(JaehoSpringBootApplication.class, args);
