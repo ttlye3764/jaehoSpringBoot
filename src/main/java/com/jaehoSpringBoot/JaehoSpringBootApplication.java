4fc46e4e8f0f62df51aa13be349305e49f77e041
jaeho <ttlye9967@gmail.com>
2023-02-26 11:28:14 +0900

    Front Controller 제거 DispatcherServlet 생성

--- src/main/java/com/jaehoSpringBoot/JaehoSpringBootApplication.java
+++ src/main/java/com/jaehoSpringBoot/JaehoSpringBootApplication.java
@@ -21,10 +21,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.util.StringUtils;
+import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.ServletContext;
@@ -44,7 +46,7 @@ public class JaehoSpringBootApplication {
         // 1. Bean이 뭐가 들어갈지
         // 2. resource에 접근하는 방법
         // 3. 이벤트에 뭐 어쩌고 하는 방법 등등.
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
@@ -52,34 +54,8 @@ public class JaehoSpringBootApplication {
         // tomcat 시작.
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-             servletContext.addServlet("frontController", new HttpServlet() {
-                 @Override
-                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                     //  각종 공통 기능 처리
-                     // - 인증, 보안, 다국어 등.
-                     //
-                     if ("/hello".equals(req.getRequestURI()) // 매핑
-                             && req.getMethod().equals(HttpMethod.GET.name())) {
-
-                         // 바인딩
-                         String name = req.getParameter("name");
-                         HelloController helloController = applicationContext.getBean(HelloController.class);
-                         String ret = helloController.hello(name);
-
-                         resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                         resp.getWriter().println(ret);
-
-                     } else if ("/file".equals(req.getRequestURI())) {
-                         // 파일 가져올때.
-                         MultipartResolver resolver = new CommonsMultipartResolver(req.getServletContext());
-                         MultipartHttpServletRequest multipartHttpServletRequest = resolver.resolveMultipart(req);
-                         MultipartFile file = multipartHttpServletRequest.getFile("file");
-                     } else {
-                         resp.setStatus(HttpStatus.NOT_FOUND.value());
-                     }
-
-                 }
-             }).addMapping("/*");
+             servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext))
+                     .addMapping("/*");
         });
         webServer.start();
     }
